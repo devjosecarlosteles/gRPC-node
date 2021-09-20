8e7f5c1e820c4ae44fd3e726fb7a875cebfaba3f
devjosecarlosteles <devjosecarlosteles@gmail.com>
2021-09-19 22:33:50 -0300

    client and get_notes

--- src/client/client-grpc.js
+++ src/client/client-grpc.js
@@ -1,8 +1,23 @@
-const grpc = require("grpc")
-const PROTO_PATH = "../../notes.proto" // loado proto config
-const NotesService = grpc.load(PROTO_PATH).NotesService
+const grpc = require("@grpc/grpc-js")
+const protoLoader = require("@grpc/proto-loader")
 
-const client = new NotesService("loacalhost:50051", 
+const PROTO_PATH = __dirname + "/../../notes.proto" // loado proto config
+// const NotesService = grpc.load(PROTO_PATH).NotesService
+
+const packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  }
+)
+
+const notes_proto = grpc.loadPackageDefinition(packageDefinition).notes
+
+const client = new notes_proto.NoteService("localhost:50051", 
   grpc.credentials.createInsecure()) // creating calls with the server
-
+  
 module.exports = client
--- src/server.js
+++ src/server.js
@@ -1,5 +1,20 @@
-const grpc = require("grpc")
-const notesProto = grpc.load("../notes.proto") // load proto config
+const grpc = require("@grpc/grpc-js")
+const protoLoader = require("@grpc/proto-loader")
+
+const PROTO_PATH = __dirname + "/../notes.proto"
+
+const packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  }
+)
+
+const notesProto = grpc.loadPackageDefinition(packageDefinition).notes
 
 /**
  * Object that we will upload to our server. 
@@ -7,20 +22,21 @@ const notesProto = grpc.load("../notes.proto") // load proto config
  * (id, title and content).
  */
 const notes = [
-  { id: "1", title: "note1", content: "Content 1" },
-  { id: "2", title: "note2", content: "Content 2" },
-]
+  { id: '1', title: 'note1', content: 'Content 1' },
+  { id: '2', title: 'note2', content: 'Content 2' },
+]   
 
 const server = new grpc.Server() // gRPC server creation
 
+function List(_, callback) {
+  callback(null, { notes })
+}
+
 // we are passing the .proto file to the server
-server.addService(notesProto.NoteService.service, {
-  list: (_, callback) => {
-    callback(null, notes)
-  }
-})
+server.addService(notesProto.NoteService.service, { List })
 
 // uploading the server on port 50051
-server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure())
-console.log("Server running at http://127.0.0.1:50051")
-server.start()
+server.bindAsync("127.0.0.1:50051", grpc.ServerCredentials.createInsecure(), () => {
+  console.log("server running in localhost:50051")
+  server.start()
+})
